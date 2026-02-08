41112a072f6423b77823d2cedaccfa5b773a5100
Sofya Surovneva <ssurovneva@mail.ru>
2026-02-08 19:57:47 +0300

    checkbox for password visibility

--- test/src/components/LoginForm.tsx
+++ test/src/components/LoginForm.tsx
@@ -1,18 +1,27 @@
 
 
-export function LoginForm() {
+
+export function LoginForm({passwordVisibility, setPasswordVisibility}) {
     return (
         <div className=' '>
-            <form action="" className="flex flex-col w-100 h-80">
-                <input type="text" placeholder='Логин' name="" id="" className="border-2 border-[#6c5ce7] rounded-xl 
+            <form action="" className="flex flex-col w-100 h-100">
+                <input type="text" placeholder='Логин' name="" id=""
+                    className="border-2 border-[#6c5ce7] rounded-xl 
                             p-1 m-3 h-12 
                             focus:outline-none focus:ring-1 focus:ring-[#6c5ce7]"/>
-                <input type="password" placeholder='Пароль' name="" id="" className="border-2 border-[#6c5ce7] rounded-xl 
+                <input type={passwordVisibility ? 'text' : 'password'} placeholder='Пароль' name="" id=""
+                    className="border-2 border-[#6c5ce7] rounded-xl 
                             p-1 m-3 h-12
                             focus:outline-none focus:ring-1 focus:ring-[#6c5ce7]" />
+                <label htmlFor="" className="self-end mr-5"> Показать пароль
+                    <input type="checkbox" className="ml-2 " onChange={(e) => {
+                        setPasswordVisibility(e.target.checked)
+                    }} />
+                </label>
                 <button type='submit'
                     className='h-13 mt-2 w-50 self-center 
-                                border-2 border-[#6c5ce7] rounded-md bg-[#6c5ce7] hover:bg-[#8274ed] text-white transition  duration-500 ease-in-out hover:shadow-xl/40
+                                border-2 border-[#6c5ce7] rounded-md bg-[#6c5ce7] hover:bg-[#8274ed] 
+                                text-white transition  duration-500 ease-in-out hover:shadow-xl/40
                                 hover:cursor-pointer'>Войти</button>
             </form>
         </div>
